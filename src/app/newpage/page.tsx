9a77e906eaa55c3834aa59ea43b8c7aa78800c7d
awprevite <awprevite@gmail.com>
2024-12-31 02:33:05 -0500

    First route working

---
+++ src/app/newpage/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function NewPage() {
+    return (
+      <div>
+        <h2>Welcome to the New Page!</h2>
+        <p>This is a new page in the app.</p>
+      </div>
+    );
+  }
