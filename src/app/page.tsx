fc65d99e97c23bdbfd6deb386a169a16aa8fca72
awprevite <awprevite@gmail.com>
2025-01-06 22:28:34 -0500

    Set up with aws, first api working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,56 @@
 
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function LoginPage() {
 
   const[loginOpen, setLoginOpen] = useState(false);
   const[createOpen, setCreateOpen] = useState(false);
+  const[selectedRole, setSelectedRole] = useState(true); {/*true is for player*/}
 
   const router = useRouter();
 
-  const handleLogin = () => {
+  const urlLogin = "https://j08uqzcsf4.execute-api.us-east-1.amazonaws.com/main/login";
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(urlLogin, {
+        email: "awprevite",
+        password: "password",
+      });
+
+      const { statusCode, body } = response.data;
+
+      const { host_id, email, password } = body;
+
+      console.log("Host ID:", host_id);
+      console.log("Email:", email);
+      console.log("Password:", password);
+
+
+      if (statusCode === 200) {
+        alert("Login successful!");
+      } else {
+        alert("Login failed");
+      }
+    } catch (error) {
+        //console.error("Login Error:", error.response?.data || error.message);
+        alert("An unexpected error occurred. Please try again later.");
+    }
+  };
+
+
+
+    {/*
     setLoginOpen(false);
     setCreateOpen(false);
-    router.push('/newpage');
-  };
+    if(selectedRole === true){
+      router.push('/player');
+    }else{
+      router.push('host');
+    }
+    */}
 
   const toggleLogin = () => {
     setLoginOpen(!loginOpen);
@@ -22,6 +59,10 @@ export default function LoginPage() {
 
   const toggleCreate = () => {
     setCreateOpen(!createOpen);
+  }
+
+  const handleSelectedRole = (role:boolean) => {
+    setSelectedRole(role);
   }
 
   return (
@@ -33,7 +74,10 @@ export default function LoginPage() {
       {loginOpen && (
         <div className="modal-overlay">
           <div className="login-modal">
+            <h2>Login</h2>
             <div className="login-button-container">
+              <button className={selectedRole === true ? "role-button selected" : "role-button"} onClick={() => handleSelectedRole(true)}>player</button>
+              <button className={selectedRole === true ? "role-button" : "role-button selected"} onClick={() => handleSelectedRole(false)}>host</button>
               <button className="close-button" onClick={toggleLogin}>Back</button>
             </div>
             <div className="login-input-container">
@@ -54,7 +98,10 @@ export default function LoginPage() {
       {createOpen && (
         <div className="modal-overlay">
         <div className="login-modal">
+          <h2>Create Account</h2>
           <div className="login-button-container">
+            <button className={selectedRole === true ? "role-button selected" : "role-button"} onClick={() => handleSelectedRole(true)}>player</button>
+            <button className={selectedRole === true ? "role-button" : "role-button selected"} onClick={() => handleSelectedRole(false)}>host</button>
             <button className="close-button" onClick={() => {toggleLogin(); toggleCreate();}}>Back</button>
           </div>
           <div className="login-input-container">
@@ -65,7 +112,7 @@ export default function LoginPage() {
             <input className="login-input" type="password" placeholder="password" id="password" name="password" required />
 
             <label className="login-label">Confirm Password</label>
-            <input className="login-input" type="password" placeholder="confirm password" id="password" name="password" required />
+            <input className="login-input" type="password" placeholder="confirm password" id="confirm-password" name="confirm-password" required />
           </div>
           <div className="login-button-container">
             <button className="login-button" onClick={handleLogin}>Create Account</button>
