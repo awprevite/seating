153c81da9f3eee72b49c4ab2735690dee27929f2
awprevite <awprevite@gmail.com>
2025-01-09 21:29:26 -0500

    Almost done, need to fix table viewing player side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,19 +19,19 @@ export default function App() {
   const[confirmPassword, setConfirmPassword] = useState<string>("");
 
   // Host variables
-  const [totalSeats, setTotalSeats] = useState<number>(0);
+  const [totalSeats, setTotalSeats] = useState<number>(2);
   const [hostKey, setHostKey] = useState<string>("");
   const [selectedDate, setSelectedDate] = useState<string>("");
   const [selectedTime, setSelectedTime] = useState<string>("");
   const [selectedGame, setSelectedGame] = useState<number | null>(null);
   const [games, setGames] = useState<any[]>([]);
+  const [createGameOpen, setCreateGameOpen] = useState(false);
 
   // Player variables
   const [selectedGamePlayer, setSelectedGamePlayer] = useState<number | null>(null);
   const [selectedDatePlayer, setSelectedDatePlayer] = useState<string>("");
   const [hostKeyPlayer, setHostKeyPlayer] = useState<string>("");
-
-  const names = ["one", "two"]; // placeholder
+  const [usernames, setUsernames] = useState<string[]>([]);
 
   // API endpoints
   const urlLogin = "https://j08uqzcsf4.execute-api.us-east-1.amazonaws.com/main/login";
@@ -39,6 +39,9 @@ export default function App() {
   const urlCreateTable = "https://j08uqzcsf4.execute-api.us-east-1.amazonaws.com/main/createTable";
   const urlGetHostTableInfo = "https://j08uqzcsf4.execute-api.us-east-1.amazonaws.com/main/getHostTableInfo";
   const urlDeleteTable = "https://j08uqzcsf4.execute-api.us-east-1.amazonaws.com/main/deleteTable";
+  const urlSearchGames = "https://j08uqzcsf4.execute-api.us-east-1.amazonaws.com/main/searchGames";
+  const urlJoinGame = "https://j08uqzcsf4.execute-api.us-east-1.amazonaws.com/main/joinGame";
+  const urlGetGameInfo = "https://j08uqzcsf4.execute-api.us-east-1.amazonaws.com/main/getTableInfo"
 
   // API functions
   const handleCreateGame = async () => {
@@ -57,12 +60,51 @@ export default function App() {
       });
       const { statusCode } = response.data;
       alert(statusCode === 200 ? "Successfully Created Game and Seats!" : "Failed to create game and seats");
+      setCreateGameOpen(false);
     } catch (error) {
       alert("An unexpected error occurred. Please try again later.");
     } finally {
       getHostGameInfo();
     }
   };
+
+  const handleJoinGame = async () => {
+    try {
+      const response = await axios.post(urlJoinGame, {
+        username: username,
+        game_id: selectedGame,
+      });
+
+      const { statusCode, body } = response.data;
+
+      if (statusCode === 200) {
+        alert("Joined");
+      } else {
+        alert("Could Not Join");
+      }
+    } catch (error) {
+        alert("An unexpected error occurred. Please try again later.");
+    }
+
+    getGameInfo();
+  };
+
+  const handleSearchGames = async () => {
+    try {
+      const response = await axios.post(urlSearchGames, { 
+        host_key: hostKey, 
+        start_time_date: selectedDate
+      });
+      const { statusCode, body } = response.data;
+      if (statusCode === 200) {
+        setGames(body);
+      } else {
+        alert("Failed to retrieve game information");
+      }
+    } catch (error) {
+      alert("An unexpected error occurred. Please try again later.");
+    };
+  }
 
   const handleDeleteGame = async () => {
     try {
@@ -98,8 +140,30 @@ export default function App() {
     };
   }
 
-  const handleLogin = async () => {
+  const getGameInfo = async () => {
+    console.log(selectedGame)
+    try {
+      const response = await axios.post(urlGetHostTableInfo, { 
+        game_id: selectedGame
+      });
+      const { statusCode, game, seat } = response.data;
 
+      console.log(statusCode);
+      console.log(seat);
+  
+      if (statusCode === 200) {
+        (seat as { player_username: string }[]).forEach((seatItem) => {
+          usernames.push(seatItem.player_username)
+        });
+      } else {
+        alert("Failed to retrieve game information");
+      }
+    } catch (error) {
+      alert("An unexpected error occurred. Please try again later.");
+    }
+  };
+
+  const handleLogin = async () => {
     try {
       const response = await axios.post(urlLogin, {
         role: selectedRole,
@@ -108,9 +172,7 @@ export default function App() {
       });
 
       const { statusCode, body } = response.data;
-
       const { id, returnedUsername, returnedPassword } = body;
-
       console.log("ID:", id);
       console.log("Username:", returnedUsername);
       console.log("Password:", returnedPassword);
@@ -124,6 +186,7 @@ export default function App() {
           setPlayerOpen(true);
         }else{
           setHostOpen(true);
+          getHostGameInfo();
         }
       } else {
         alert("Login failed");
@@ -135,7 +198,6 @@ export default function App() {
 
   const handleCreateAccount = async () => {
     try {
-
       if(password != confirmPassword){
         alert("passwords do not match")
         return
@@ -148,9 +210,7 @@ export default function App() {
       });
 
       const { statusCode, body } = response.data;
-
       const { id, returnedUsername, returnedPassword } = body;
-
       console.log("ID:", id);
       console.log("username:", returnedUsername);
       console.log("Password:", returnedPassword);
@@ -183,26 +243,36 @@ export default function App() {
   const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setConfirmPassword(e.target.value);
   };
-
   const toggleLogin = () => {
     setLoginOpen(!loginOpen);
   };
-
   const toggleCreate = () => {
     setCreateOpen(!createOpen);
   }
-
+  const toggleCreateGame = () => {
+    setCreateGameOpen(!createGameOpen);
+  }
   const handleLogout = () => {
     setHomeOpen(true);
     setHostOpen(false);
     setPlayerOpen(false);
+    setUsername("");
+    setPassword("");
+    setSelectedRole("player");
+    setConfirmPassword("");
+    setTotalSeats(2);
+    setHostKey("");
+    setSelectedDate("");
+    setSelectedTime("");
+    setSelectedGame(null);
+    setGames([]);
   }
 
   // Other functions
 
   const renderGameList = () => (
     <div>
-      <h1>Select a Game</h1>
+      <h1>Your Games</h1>
       <ul>
         {games.map((game) => (
           <li key={game.game_id}>
@@ -228,7 +298,7 @@ export default function App() {
         <ul>
           {game.seats.map((seat: any) => (
             <li key={seat.seat_id}>
-              Seat ID: {seat.seat_id}, Occupied: {seat.player_id}
+              Seat Number: {seat.seat_num}, Occupied: {seat.player_id ? seat.player_id : "no"}
             </li>
           ))}
         </ul>
@@ -272,7 +342,6 @@ export default function App() {
             </div>
           </div>
         )}
-        
         {createOpen && (
           <div className="modal-overlay">
           <div className="login-modal">
@@ -297,112 +366,102 @@ export default function App() {
             </div>
           </div>
         </div>
-        )}    
+        )}
       </div>
     );
   }
 
   const playerView = () => {
-    if (selectedDate === ""){
-      return(
-        <div>
-          <input type="text" value={hostKey} onChange={handleKeyChange}></input>
-          <div className="date-picker-container">
-            <label htmlFor="select-date">Select Date:</label>
-            <input 
-              type="date" 
-              id="select-date" 
-              value={selectedDate} 
-              onChange={handleDateChange} 
-            />
-          </div>
-          <button onClick={() => setSelectedDate("")}>Clear</button>
+
+    return(
+      <div>
+        <label>Enter Host Key:</label>
+        <input type="text" value={hostKey} onChange={handleKeyChange}></input>
+        <label>Filter by Date</label>
+        <div className="date-picker-container">
+          <label htmlFor="select-date">Select Date:</label>
+          <input type="date" id="select-date" value={selectedDate} onChange={handleDateChange}/>
         </div>
-      );
-    }
-  
-    if (selectedGame === null) {
-      return (
-        <div>
-            <input type="text" value={hostKey} onChange={handleKeyChange}></input>
-            <div className="date-picker-container">
-              <label htmlFor="select-date">Select Date:</label>
-              <input type="date" id="select-date" value={selectedDate} onChange={handleDateChange}/>
+        <button onClick={() => setSelectedDate("")}>Clear</button>
+        <button onClick={() => handleSearchGames()}>Search</button>
+        <label>Available Games</label>
+        <ul>
+          {games.map((game) => (
+            <li key={game.game_id}>
+              <button onClick={() => {setSelectedGame(game.game_id); getGameInfo();}}>
+                {game.game_id}
+              </button>
+            </li>
+          ))}
+        </ul>
+        {selectedGame && (
+          <div className="container">
+          <button onClick={() => setSelectedGame(null)}>Back to Game List</button>
+          <h1>Details for {games.find((game) => game.game_id === selectedGame)?.game_id}</h1>
+          <div className="table-container">
+            <div className="table">
+              {usernames.map((username, index) => {
+                const angle = (360 / usernames.length) * index;
+                const x = Math.ceil(250 + 250 * Math.cos((angle * Math.PI) / 180));
+                const y = Math.ceil(125 + 125 * Math.sin((angle * Math.PI) / 180));
+                return (
+                  <div
+                    key={index}
+                    className={`name position-${index + 1}`}
+                    style={{position: 'absolute', left: `${x}px`, top: `${y}px`, transform: `translate(-50%, -50%)`,}}>
+                    <button onClick={() => handleNameClick(username)}>{username}</button>
+                  </div>
+                );
+              })}
             </div>
-            <button onClick={() => setSelectedDate("")}>Clear</button>
-          <div className="table-list">
-            <h1>Select a Game</h1>
-            <ul>
-              {games.map((game) => (
-                <li key={game.game_id}>
-                  <button onClick={() => setSelectedGame(game.game_id)}>{game.game_id}</button>
-                </li>
-              ))}
-            </ul>
           </div>
+          <button onClick={handleJoinGame}>Join Game</button>
         </div>
-      );
-    }
-  
-    // Table Details Screen
-    return (
-      <div className="container">
-        <button onClick={() => setSelectedGame(null)}>Back to Game List</button>
-        <h1>Details for {games.find((game) => game.game_id === selectedGame)?.game_id}</h1>
-        
-        <div className="table-container">
-          <div className="table">
-            {names.map((name, index) => {
-              const angle = (360 / names.length) * index;
-              const x = Math.ceil(250 + 250 * Math.cos((angle * Math.PI) / 180));
-              const y = Math.ceil(125 + 125 * Math.sin((angle * Math.PI) / 180));
-  
-              return (
-                <div
-                  key={index}
-                  className={`name position-${index + 1}`}
-                  style={{
-                    position: 'absolute',
-                    left: `${x}px`,
-                    top: `${y}px`,
-                    transform: `translate(-50%, -50%)`,
-                  }}
-                >
-                  <button onClick={() => handleNameClick(name)}>{name}</button>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+      )}
       </div>
     );
-  }
+  };
+
 
   const hostView = () => {
     return (
       <div>
-        <button onClick={handleLogout}>Log out</button>
-        <label>Total Seats</label>
-        <select onChange={(e) => setTotalSeats(Number(e.target.value))} value={totalSeats}>
-          {[...Array(8)].map((_, index) => (
-            <option key={index} value={index + 2}>
-              {index + 2}
-            </option>
-          ))}
-        </select>
-        <label>Key</label>
-        <input type="text" value={hostKey} onChange={(e) => setHostKey(e.target.value)}/>
-        <label>Date</label>
-        <input type="date" id="select-date" value={selectedDate} onChange={handleDateChange}/>
-        <label>Time</label>
-        <select onChange={(e) => setSelectedTime(e.target.value)} value={selectedTime}>
-          {[...Array(24)].map((_, index) => (
-            <option key={index} value={index}>
-              {String(index).padStart(2, '0')}
-            </option>
-          ))}
-        </select>
-        <button onClick={handleCreateGame}>"Create Game"</button>
+        <div className="login-button-container">
+          <button onClick={handleLogout}>Log out</button>
+          <button onClick={toggleCreateGame}>Create Game</button>
+        </div>
+        {createGameOpen && (
+          <div className="modal-overlay">
+            <div className="login-modal">
+              <div className="login-button-container">
+                <button onClick={toggleCreateGame}>Back</button>
+              </div>
+              <div className="login-input-container">
+                <label>Total Seats</label>
+                <select onChange={(e) => setTotalSeats(Number(e.target.value))} value={totalSeats}>
+                  {[...Array(8)].map((_, index) => (
+                    <option key={index} value={index + 2}>
+                      {index + 2}
+                    </option>
+                  ))}
+                </select>
+                <label>Key</label>
+                <input type="text" value={hostKey} onChange={(e) => setHostKey(e.target.value)}/>
+                <label>Date</label>
+                <input type="date" id="select-date" value={selectedDate} onChange={handleDateChange}/>
+                <label>Time</label>
+                <select onChange={(e) => setSelectedTime(e.target.value)} value={selectedTime}>
+                  {[...Array(24)].map((_, index) => (
+                    <option key={index} value={index}>
+                      {String(index).padStart(2, '0')}
+                    </option>
+                  ))}
+                </select>
+                <button onClick={handleCreateGame}>Create Game</button>
+              </div>
+            </div>
+          </div>
+        )}
         {selectedGame === null && renderGameList()}
         {selectedGame !== null && renderGameDetails()}
       </div>
